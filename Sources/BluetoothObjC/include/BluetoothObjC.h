29c513bcdc398e6ba5822a4c62a3512e84d876a0
Maxime Peschard <maxime.peschard@gmail.com>
2018-09-23 15:20:35 +0200

    [#1] Add private API

---
+++ Sources/BluetoothObjC/include/BluetoothObjC.h
@@ -0,0 +1,12 @@
+@import IOBluetooth;
+
+// Private APIs
+
+// https://github.com/w0lfschild/macOS_headers/blob/master/macOS/Frameworks/IOBluetooth/6.0.8f6/IOBluetoothDevice.h
+@interface IOBluetoothDevice (Private)
+@property(nonatomic) unsigned char batteryPercentCombined;
+@property(nonatomic) unsigned char batteryPercentCase;
+@property(nonatomic) unsigned char batteryPercentRight;
+@property(nonatomic) unsigned char batteryPercentLeft;
+@property(nonatomic) unsigned char batteryPercentSingle;
+@end
