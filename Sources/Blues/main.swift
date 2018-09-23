c4a75eee12d0b2a5f87fc7d4444f6b904662ee52
Maxime Peschard <maxime.peschard@gmail.com>
2018-09-23 18:56:58 +0200

    Bump version to 0.2.0

--- Sources/Blues/main.swift
+++ Sources/Blues/main.swift
@@ -2,7 +2,7 @@ import SwiftCLI
 
 let bluesCli = CLI(
     name: "blues",
-    version: "0.1.0",
+    version: "0.2.0",
     description: "Blues - Manage Bluetooth connections with paired devices"
 )
 bluesCli.commands = [
